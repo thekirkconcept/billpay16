bb6b87c82beead84c93e1722540289cc9f12fc76
Christoph The Artist <thekirkconcept@gmail.com>
2016-11-13 07:04:05 -0800

    Registration page

--- bp_register.php
+++ bp_register.php
@@ -5,13 +5,13 @@ include('header.html');
 
 // Create variables of column names from database
 $userID = $_POST["userID"];
-$ufname = $_POST["u_fname"];
-$ulname = $_POST["u_lname"];
-$uemail = $_POST["email"];
-$uphone = $_POST["u_phone"];
 $usernameID = $_POST["username"];
+$uemail = $_POST["email"];
 $upassword = $_POST["u_password"];
 $upasswordv = $_POST["u_pw_verify"];
+$uphone = $_POST["u_phone"];
+$ufname = $_POST["u_fname"];
+$ulname = $_POST["u_lname"];
 $uaddress = $_POST["u_address"];
 $uaddress2 = $_POST["u_address2"];
 $ucity = $_POST["u_city"];
@@ -19,8 +19,8 @@ $ustate = $_POST["u_state"];
 $uzipcode = $_POST["u_zipcode"];
 
 // Query to insert data into database table
-$sql="INSERT INTO users(userID,u_fname,u_lname,email,u_phone,username,u_password,u_pw_verify,u_address,u_address2,u_city,u_state,u_zipcode) 
-VALUES('".$userID."','".$ufname."','".$ulname."','".$uemail."','".$uphone."','".$usernameID."','".$upassword."','".$upasswordv."','".$uaddress."','".$uaddress2."','".$ucity."','".$ustate."','".$uzipcode."')";
+$sql="INSERT INTO users(userID,username,email,u_password,u_pw_verify,u_phone,u_fname,u_lname,u_address,u_address2,u_city,u_state,u_zipcode) 
+VALUES('".$userID."','".$usernameID."','".$uemail."','".$upassword."','".$upasswordv."','".$uphone."','".$ufname."','".$ulname."','".$uaddress."','".$uaddress2."','".$ucity."','".$ustate."','".$uzipcode."')";
 
 if (mysqli_query($dbcon, $sql)) {
       //header("Location: bank_details.html");
