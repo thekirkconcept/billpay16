2d795aca7d716e5c028b88a6b9aa04fc290ed38a
Christoph The Artist <thekirkconcept@gmail.com>
2016-11-13 07:07:23 -0800

    Page Template

---
+++ templates/page.php
@@ -0,0 +1,20 @@
+<?php
+
+include("includes/header.php");
+?>
+
+<meta name="Keywords" content="KEYWORD1, KEYWORD2, KEYWORD3">
+<meta name="Description" content="BRIEF DESCRIPTION OF PAGE HERE.">
+<title>Title here</title>
+
+<?php
+include("includes/nav.php"); 
+?>
+
+<div id="content">
+CONTENT HERE
+</div>
+
+<?php
+include("includes/footer.php");
+?>
