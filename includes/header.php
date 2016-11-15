9e6a772f96ce2b5162aa67a7b17eb310f57a0c02
Christoph The Artist <thekirkconcept@gmail.com>
2016-11-15 01:42:18 -0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Christopher Jones">
+
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+          
+        <link href="css/style.css" rel="stylesheet" type="text/css">
